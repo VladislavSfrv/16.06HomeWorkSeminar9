e1c470da934066cc52c0431f4d16d3faecd10346
unknown <vladislavsafarov04@mail.ru>
2023-06-17 13:57:40 +0300

    Added Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задайте значение N. 
+//Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+PrintNaturalNumber(num);
+
+void PrintNaturalNumber(int num)
+{
+    if (num > 0)
+    {
+        Console.Write($"{num} ");
+        if (num == 1) return;
+        PrintNaturalNumber(num - 1);
+    }else Console.WriteLine("Введите число больше 0.");
+}
